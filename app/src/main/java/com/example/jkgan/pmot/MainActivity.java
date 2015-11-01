f8965d8f2a3d2910b1fe2377430904415e03746a
Jun Kai <kuhn96@gmail.com>
2015-11-01 21:16:07 +0800

    done authentication with Rails API

--- app/src/main/java/com/example/jkgan/pmot/MainActivity.java
+++ app/src/main/java/com/example/jkgan/pmot/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.jkgan.pmot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText txtEmail = (EditText) findViewById(R.id.editTextEmail);
-        EditText txtPassword = (EditText) findViewById(R.id.editTextPassword);
-        Button btnlogin = (Button) findViewById(R.id.buttonLogin);
+        TextView txtToken = (TextView) findViewById(R.id.textViewToken);
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
+        txtToken.setText(getIntent().getStringExtra("TOKEN"));
     }
 }
