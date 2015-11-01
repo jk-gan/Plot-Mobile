63ac335d70a2aa95f45ad1bb5aa67144ddee3492
Jun Kai <kuhn96@gmail.com>
2015-11-01 22:20:39 +0800

    done qr code scanner

--- app/src/main/java/com/example/jkgan/pmot/MainActivity.java
+++ app/src/main/java/com/example/jkgan/pmot/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.jkgan.pmot;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button scannerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         TextView txtToken = (TextView) findViewById(R.id.textViewToken);
-
         txtToken.setText(getIntent().getStringExtra("TOKEN"));
+
+        scannerButton = (Button) findViewById(R.id.buttonScanner);
+        scannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), QRcodeScanner.class);
+                startActivity(intent);
+            }
+        });
     }
 }
